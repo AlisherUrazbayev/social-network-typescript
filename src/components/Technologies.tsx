bdb7834b131ba27a5943994dc91dbf41b0d35388
Alisher Urazbayev <Alisher1518204240@gmail.com>
2022-05-21 13:13:55 -0400

    Initializing project

---
+++ src/components/Technologies.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Technologies = () => {
+    return (
+        <div>
+            <ul>
+                <li>JavaScript</li>
+                <li>HTML</li>
+                <li>CSS</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Technologies;
