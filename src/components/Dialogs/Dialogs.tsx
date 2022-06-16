5d6a46aa89d68233d253be854b8429aea6dfff65
Alisher Urazbayev <Alisher1518204240@gmail.com>
2022-06-16 02:43:01 -0400

    Refactor Dialogs component and create ProfileInfo component

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,23 +3,43 @@ import styles from './Dialogs.module.css';
 import {NavLink} from "react-router-dom";
 
 
+type DialogItemPropsType = {
+    _id: string
+    name: string
+}
+
+const DialogItem = ({_id, name}: DialogItemPropsType) => {
+    return (
+        <div>
+            <NavLink to={`./${_id}`}>{name}</NavLink>
+        </div>
+    )
+}
+
+type MessagePropsType = { text: string }
+
+const Message = ({text}: MessagePropsType) => {
+    return <div>{text}</div>
+}
+
+
 function Dialogs() {
     return (
-            <div className={styles.container}>
+        <div className={styles.container}>
 
-                <div className={styles.dialogs}>
-                    <div><NavLink to={"./person-1"}>Person-1</NavLink></div>
-                    <div><NavLink to={"./person-2"}>Person-2</NavLink></div>
-                    <div><NavLink to={"./person-3"}>Person-3</NavLink></div>
-                    <div><NavLink to={"./person-4"}>Person-4</NavLink></div>
-                </div>
-                <div className={styles.messages}>
-                    <div>Message-1</div>
-                    <div>Message-2</div>
-                    <div>Message-3</div>
-                    <div>Message-4</div>
-                </div>
+            <div className={styles.dialogs}>
+                <DialogItem _id={'1'} name={'Person-1'}/>
+                <DialogItem _id={'2'} name={'Person-2'}/>
+                <DialogItem _id={'3'} name={'Person-3'}/>
+                <DialogItem _id={'4'} name={'Person-4'}/>
             </div>
+            <div className={styles.messages}>
+                <Message text={'Message-1'}/>
+                <Message text={'Message-2'}/>
+                <Message text={'Message-3'}/>
+                <Message text={'Message-4'}/>
+            </div>
+        </div>
     );
 }
 
